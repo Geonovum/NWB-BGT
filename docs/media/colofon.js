6bda64afdc8d545e39cebac309fe40281d507c16
Arnoud de Boer <a.deboer@geonovum.nl>
2018-07-12 09:13:33 +0200

    Update colofon.js

--- docs/media/colofon.js
+++ docs/media/colofon.js
@@ -1,8 +1,8 @@
 var respecConfig = {
-  specStatus: "GN-VV",
+  specStatus: "GN-DEF",
   specType: "HR",
   //canonicalURI: "WV",
-  publishDate: "2018-03-30",
+  publishDate: "2018-07-19",
   editors: [
     {
       name: "Arnoud de Boer",
